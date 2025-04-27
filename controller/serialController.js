5fa8625fcc92ccda7ea5f57b7227363ce8f3731f
Poornima3787-developer <taehyungpurple02@gmail.com>
2025-04-27 20:48:28 +0530

    crud_sequelize

--- controller/serialController.js
+++ controller/serialController.js
@@ -1,53 +1,52 @@
 const connection = require('../utils/db-connection');
 const db=require('../utils/db-connection');
+const Serial=require('../models/serial');
 
-const addEntries=(req,res)=>{
-const {email,name}=req.body;
-const insertQuery='INSERT INTO Serial (email,name) VALUES (?,?)';
-db.execute(insertQuery,[email,name],(err)=>{
-  if(err){
-    console.log(err.message);
-    res.status(500).send(err.message);
-    return;
+const addEntries=async (req,res)=>{
+  try {
+    const {email,name}=req.body;
+    const serial=await Serial.create({
+      email:email,
+      name:name
+    });
+    res.status(200).send(`User with name:${name} is created`)
+  } catch (error) {
+    res.status(500).send('Unable to make an entry');
   }
-  console.log("Value has been inserted");
-  res.status(200).send(`Student with name ${name} successfully added`);
-}) 
 }
 
-const updateEntry=(req,res)=>{
-  const {id}=req.params;
+const updateEntry=async (req,res)=>{
+  try {
+    const {id}=req.params;
   const {name}=req.body;
-  const updateQuery="UPDATE serial set name=? where id=?";
-  db.execute(updateQuery,[name,id],(err,result)=>{
-    if(err){
-      console.log(err.message);
-      res.status(500).send(err.message);
-      return;
-    }
-    if(result.affectedRows === 0){
-      res.status(404).send("Serial is not found");
-      return;
-    }
-    res.status(200).send("User has been created");
-  })
+  const serial=await Serial.findByPk(id);
+  if(!serial){
+    res.status(404).send("User is not found");
+  }
+  serial.name=name;
+  await serial.save();
+  res.status(200).send(`User has been updated`)
+  } catch (error) {
+    res.status(500).send('User cannot be updated');
+  }
 }
 
-const deleteEntry=(req,res)=>{
-  const {id}=req.params;
-  const deleteQuery="DELETE FROM serial where id=?";
-  db.execute(deleteQuery,[id],(err,result)=>{
-    if(err){
-      console.log(err.message);
-      res.status(500).send(err.message);
-      return;
+const deleteEntry=async (req,res)=>{
+  try {
+    const {id}=req.params; 
+    const serial=await Serial.destroy({
+      where:{
+        id:id
+      }
+    })
+    if(!serial){
+      res.status(404).send("User is not found");
     }
-    if(result.affectedRows === 0){
-      res.status(404).send("Serial is not found");
-      return;
-    }
-    res.status(200).send(`User has been deleted id ${id}`);
-  })
+    res.status(200).send(`User has been deleted`)
+
+  } catch (error) {
+    res.status(500).send('Error encountered while deleting');
+  }
 }
 
 module.exports={
